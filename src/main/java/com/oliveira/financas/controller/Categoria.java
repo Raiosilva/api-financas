cb227e960192c26cd171fd67382e0d7c35a12a0b
Raiosilva <raimundopi025@gmail.com>
2019-08-02 18:16:25 -0300

    rest testado localhost

---
+++ src/main/java/com/oliveira/financas/controller/Categoria.java
@@ -0,0 +1,15 @@
+package com.oliveira.financas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class Categoria {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest está funcionando";
+	}
+}
