926bed23e249d41c353f11bdc53078f2024dcf88
Raiosilva <raimundopi025@gmail.com>
2019-08-02 18:40:18 -0300

    Testando o model Categoria

---
+++ src/main/java/com/oliveira/financas/controller/CategoriaController.java
@@ -0,0 +1,28 @@
+package com.oliveira.financas.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oliveira.financas.model.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Geladeira");
+		Categoria cat2 = new Categoria(2, "Computador");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+}
