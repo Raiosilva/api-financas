35b1cef0b1acfbe31a94b11760ad7b24a5371c80
Raiosilva <raimundopi025@gmail.com>
2019-08-05 21:23:01 -0300

    Endpoiint /clientes{id} disponivel

---
+++ src/main/java/com/oliveira/financas/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.oliveira.financas.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.oliveira.financas.model.Cliente;
+import com.oliveira.financas.repositories.ClienteRepository;
+import com.oliveira.financas.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
